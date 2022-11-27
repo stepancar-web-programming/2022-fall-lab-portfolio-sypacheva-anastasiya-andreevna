9fd37000d1b2d83a3cf3b6892d80d2ef96e462b3
Anastasias35 <71524176+Anastasias35@users.noreply.github.com>
2022-11-27 14:01:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function changeSlidesLeft() {
+  let indexBlock = 0;
+  const slides = document.getElementsByClassName('slide');
+  for (let i = 0; i < slides.length; i += 1) {
+    if (slides[i].style.display === 'block') {
+      if (i === 0) {
+        indexBlock = slides.length - 1;
+      } else indexBlock = i - 1;
+      slides[i].style.display = 'none';
+      slides[indexBlock].style.display = 'block';
+      break;
+    }
+  }
+}
+
+function changeSlidesRight() {
+  let indexBlock = 0;
+  const slides = document.getElementsByClassName('slide');
+  for (let i = 0; i < slides.length; i += 1) {
+    if (slides[i].style.display === 'block') {
+      if (i + 1 === slides.length) {
+        indexBlock = 0;
+      } else indexBlock = i + 1;
+      slides[i].style.display = 'none';
+      slides[indexBlock].style.display = 'block';
+      break;
+    }
+  }
+}
